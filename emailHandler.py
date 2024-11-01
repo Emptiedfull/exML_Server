f56ea2e544fede3ce6e978d3baecf6f9e95f75b5
empty <atharavagarwal11@gmail.com>
2024-11-01 20:55:09 +0530

    pls

---
+++ emailHandler.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = "587"
+SMTP_USERNAME  = "atharavagarwal14@gmail.com"
+SMTP_PASSWORD = "wgss sexj oytf adzq"
+EMAIL_FROM = "atharavagarwal14@gmail.com"
+
+
+
+
+def sendEmail(token,email):
+    try:
+        server = smtplib.SMTP(SMTP_SERVER,SMTP_PORT,)
+        server.starttls()
+        server.login(SMTP_USERNAME,SMTP_PASSWORD)
+
+        msg = MIMEMultipart()
+        msg['From'] = EMAIL_FROM
+        msg['To'] = email
+        msg['Subject'] = "EXML token"
+
+        body = f'Your token is: {token}, Input your token in site to check your position in queue'
+        msg.attach(MIMEText(body,'plain'))
+
+        text = msg.as_string()
+    
+        server.sendmail(EMAIL_FROM,email,text)
+        server.quit()
+    except smtplib.SMTPException as e:
+
+       return "invalid email"
